3168d6ceb5b5d5872008b0a153b01b89f981cb55
hollidavis <hollidavis19@gmail.com>
2021-07-15 16:26:28 -0600

    added stylesheet & style adjustments

--- app/Models/Plant.js
+++ app/Models/Plant.js
@@ -15,7 +15,7 @@ export default class Plant {
   get Template() {
     return `
 <div class="col-md-3 m-2 p-0">
-  <div class="bg-secondary rounded">
+  <div class="bg-primary text-dark rounded">
       <div>
           <img class="plant-img rounded-top"
               src="assets/img/${this.name}.jfif"
@@ -29,7 +29,7 @@ export default class Plant {
           </div>
           <div class="d-flex justify-content-around pb-2">
               <p>In Stock: ${this.stockCount}</p>
-              <button type="button" class="btn btn-primary" onclick="app.plantsController.buyPlant('${this.name}')" >Add to Cart</button>
+              <button type="button" class="btn btn-light" onclick="app.plantsController.buyPlant('${this.name}')" >Add to Cart</button>
           </div>
       </div>
   </div>
